4a0f0bb22fa5b64aa6a1506783c92976fdd7da14
develop@defipay.biz <develop@defipay.biz>
2022-05-21 16:15:57 +0800

    测试环境密钥更新

--- Config.php
+++ Config.php
@@ -9,7 +9,7 @@ class Config
     ];
     const SANDBOX = [
         "host" => "https://api-test.defipay.biz",
-        "defipayPub" => "02adb46f0c10b5ec51d0df2183a812fdf7b330ef2c948e36cdb479f1af73a22753"
+        "defipayPub" => "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4"
     ];
 }
 
